6936fe754a6493e4509e393c43f30a05301c4d91
jeongminji4490 <hamins97@naver.com>
2023-02-24 06:33:56 +0900

    add movename strings

---
+++ app/src/main/java/com/example/airpollution/MoveName.kt
@@ -0,0 +1,73 @@
+package com.example.airpollution
+
+const val SEOUL_MOVE_NAME = "서울특별시 전 지역"
+
+const val BUSAN_SOUTHERN_MOVE_NAME = "동구, 서구, 남구, 중구, 수영구, 영도구, 해운대구"
+const val BUSAN_EASTERN_MOVE_NAME = "기장군"
+const val BUSAN_WESTERN_MOVE_NAME = "강서구, 북구, 사상구, 사하구"
+const val BUSAN_CENTRAL_MOVE_NAME = "금정구, 동래구, 연제구, 부산진구"
+
+const val DAEGU_MOVE_NAME = "대구광역시 전 지역"
+
+const val INCHEON_GANGHWA_MOVE_NAME = "강화군"
+const val INCHEON_SOUTHEAST_MOVE_NAME = "계양구, 부평구, 미추홀구, 연수구, 남동구"
+const val INCHEON_WESTERN_MOVE_NAME = "서구, 중구(영종도 ․ 용유도 제외), 동구"
+const val INCHEON_YEONGJONG_MOVE_NAME = "중구(영종도·용유도), 옹진군(영흥)"
+
+const val GWANJU_MOVE_NAME = "광주광역시 전 지역"
+const val DAEJEONG_EASTERN_MOVE_NAME = "동구, 대덕구, 중구"
+const val DAEJEONG_WESTERN_MOVE_NAM = "서구, 유성구"
+
+const val WOOLSAN_MOVE_NAME = "울산광역시 전 지역"
+
+const val SEJONG_MOVE_NAME = "세종시 전 권역"
+
+const val GYEONGGI_SOUTHERN_MOVE_NAME = "용인시, 평택시, 안성시, 이천시, 여주시"
+const val GYEONGGI_EASTERN_MOVE_NAME = "남양주시, 구리시, 하남시, 광주시, 성남시, 가평군, 양평군"
+const val GYEONGGI_NORTHERN_MOVE_NAME = "김포시, 고양시, 의정부시, 파주시, 연천군, 양주시, 동두천시, 포천시"
+const val GYEONGGI_CENTRAL_MOVE_NAME = "수원시, 안산시, 안양시, 부천시, 시흥시, 광명시, 군포시, 의왕시, 과천시, 화성시, 오산시"
+
+const val GANGWON_YEONGSEO_NORTHERN_MOVE_NAME = "춘천시, 철원군, 양구군, 홍천군, 화천군, 인제군"
+const val GANGWON_YEONGSEO_SOUTHERN_MOVE_NAME = "원주시, 횡성군, 평창군, 영월군, 정선군"
+const val GANGWON_YEONGDONG_NORTHERN_MOVE_NAME = "고성군, 양양군, 속초시"
+const val GANGWON_YEONGDONG_SOUTHERN_MOVE_NAME = "강릉시, 동해시, 삼척시, 태백시"
+
+const val CHUNGBUK_NORTHERN_MOVE_NAME = "충주시, 제천시, 단양군"
+const val CHUNGBUK_CENTRAL_MOVE_NAME = "청주시, 증평군, 진천군, 괴산군, 음성군"
+const val CHUNGBUK_SOUTHERN_MOVE_NAME = "보은군, 옥천군, 영동군"
+
+const val CHUNGNAM_EASTSOUTH_MOVE_NAME = "공주시, 청양군, 부여군, 논산시, 계룡시, 금산군"
+const val CHUNGNAM_NORTHERN_MOVE_NAME = "천안시, 아산시, 당진시"
+const val CHUNGNAM_WESTERN_MOVE_NAME = "서산시, 태안군, 예산군, 홍성군, 보령시, 서천군"
+
+const val JEONBUK_CENTRAL_MOVE_NAME = "전주시, 익산시, 완주군"
+const val JEONBUK_WESTERN_MOVE_NAME = "군산시, 정읍시, 김제시, 고창군, 부안군"
+const val JEONBUK_EASTERN_MOVE_NAME = "남원시, 진안군, 무주군, 장수군, 임실군, 순창군"
+
+const val JEONNAM_EASTERN_MOVE_NAME = "여수시, 순천시, 광양시, 곡성군, 구례군, 고흥군, 보성군"
+const val JEONNAM_WESTERN_MOVE_NAME = "목포시, 해남군, 영암군, 무안군, 함평군, 영광군, 진도군, 신안군"
+const val JEONNAM_CENTRAL_MOVE_NAME = "나주시, 담양군, 화순군, 장흥군, 강진군, 장성군, 완도군"
+
+const val GYEONGBUK_EASTERN_MOVE_NAME = "포항시, 경주시, 경산시, 영천시, 청도군, 청송군, 영덕군, 영양군, 봉화군, 울진군"
+const val GYEONGBUK_WESTERN_MOVE_NAME = "구미시, 김천시, 안동시, 영주시, 상주시, 문경시, 예천군, 의성군, 군위군, 성주군, 고령군, 칠곡군"
+const val GYEONGBUK_ULLEUNG_MOVE_NAME = "울릉군"
+
+const val GYEONGNAM_GEOJE_MOVE_NAME = "거제시"
+const val GYEONGNAM_GEOCHANG_MOVE_NAME = "거창군"
+const val GYEONGNAM_GOSEONG_MOVE_NAME = "고성군"
+const val GYEONGNAM_GIMHAE_MOVE_NAME = "김해시"
+const val GYEONGNAM_NAMHEA_MOVE_NAME = "남해군"
+const val GYEONGNAM_MILLYANG_MOVE_NAME = "밀양시"
+const val GYEONGNAM_SACHEON_MOVE_NAME = "사천시"
+const val GYEONGNAM_SANCHEONG_MOVE_NAME = "산청군"
+const val GYEONGNAM_YANGSAN_MOVE_NAME = "양산시"
+const val GYEONGNAM_UIRYEONG_MOVE_NAME = "의령군"
+const val GYEONGNAM_JINJOO_MOVE_NAME = "진주시"
+const val GYEONGNAM_CHANGNYEONG_MOVE_NAME = "창녕군"
+const val GYEONGNAM_CHANGWON_MOVE_NAME = "창원시"
+const val GYEONGNAM_TONGYEONG_MOVE_NAME = "통영시"
+const val GYEONGNAM_HADONG_MOVE_NAME = "하동군"
+const val GYEONGNAM_HAMAHN_MOVE_NAME = "함안군"
+const val GYEONGNAM_HAMYANG_MOVE_NAME = "함양군"
+const val GYEONGNAM_HAPCHEONG_MOVE_NAME = "합천군"
+const val JEJU_MOVE_NAME = "제주 전 지역"
