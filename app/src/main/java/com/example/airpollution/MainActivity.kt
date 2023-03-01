5cf5b93e358b5660d25bd60ad055ba68379dbc57
jeongminji4490 <hamins97@naver.com>
2023-03-01 21:07:49 +0900

    change main screen ui

--- app/src/main/java/com/example/airpollution/MainActivity.kt
+++ app/src/main/java/com/example/airpollution/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawStyle
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -52,18 +54,29 @@ class MainActivity : AppCompatActivity() {
                 contentDescription = "main image"
             )
             Text(
-                text = "미세먼지 알리미",
+                text = "미세먼지",
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(15.dp),
-                fontSize = 29.sp,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                fontFamily = fontFamily,
+                color = Color.White
+            )
+            Text(
+                text = "알리미",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp),
+                fontSize = 32.sp,
                 fontWeight = FontWeight.Bold,
                 textAlign = TextAlign.Center,
                 fontFamily = fontFamily,
                 color = Color.White
             )
 
-            Spacer(Modifier.height(5.dp))
+            Spacer(Modifier.height(20.dp))
 
             Row (
                 modifier = Modifier.fillMaxWidth(),
@@ -131,6 +144,42 @@ class MainActivity : AppCompatActivity() {
                     .fillMaxWidth()
                     .width(1.dp)
             )
+
+            Spacer(Modifier.height(60.dp))
+
+//            Box(modifier = Modifier
+//                .width(140.dp)
+//                .height(140.dp)
+//                .padding(15.dp),
+//                contentAlignment = Alignment.Center) {
+//                Text(
+//                    modifier = Modifier
+//                        .drawBehind {
+//                            drawCircle(
+//                                color = Color.White,
+//                                radius = this.size.maxDimension
+//                            )
+//                        },
+//                    text = "초미세먼지 주의보",
+//                    color = colorResource(R.color.sky_blue),
+//                    textAlign = TextAlign.Center,
+//                    fontSize = 23.sp,
+//                    fontFamily = fontFamily,
+//                    fontWeight = FontWeight.Bold
+//                )
+//            }
+//
+//            Spacer(Modifier.height(60.dp))
+//
+//            Text(
+//                text = "2023-00-00 08:00",
+//                fontSize = 17.sp,
+//                fontWeight = FontWeight.SemiBold,
+//                textAlign = TextAlign.Left,
+//                fontFamily = fontFamily,
+//                color = Color.White
+//            )
+
         }
     }
 }
--- app/src/main/java/com/example/airpollution/common/composable/Font.kt
+++ app/src/main/java/com/example/airpollution/common/composable/Font.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.text.font.FontWeight
 import com.example.airpollution.R
 
 val fontFamily = FontFamily(
+    Font(R.font.scdream2, FontWeight.Medium),
     Font(R.font.scdream5, FontWeight.SemiBold),
     Font(R.font.scdream7, FontWeight.Bold)
 )
